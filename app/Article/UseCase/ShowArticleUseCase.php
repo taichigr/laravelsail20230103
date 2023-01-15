3726e517e99d994cddc444fac43463f2cc7874e9
taichi <taichi.01gr14@gmail.com>
2023-01-15 20:53:24 +0900

    ShowArticleUseCase

---
+++ app/Article/UseCase/ShowArticleUseCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Article\UseCase;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+
+final class ShowArticleUseCase
+{
+
+    /**
+     * 記事新規登録
+     *
+     * @throws ValidationException
+     */
+    public function handle(Article $article): array
+    {
+        return [
+            'article' => $article,
+        ];
+    }
+}
