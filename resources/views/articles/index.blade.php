1abd746eb08b151c1de8ca6a59102cd4b0f95660
taichi <taichi.01gr14@gmail.com>
2023-01-15 12:51:27 +0900

    show article index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,17 +8,19 @@
     <div class="py-12">
         @foreach ($articles as $article)
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 mb-4">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="text-gray-900 font-bold text-xl mb-2">
-                        {{ $article->title ?? '' }}
+                <a href="{{ route('articles.edit', ['article' => $article]) }}">
+                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <div class="text-gray-900 font-bold text-xl mb-2">
+                            {{ $article->title ?? '' }}
+                        </div>
+                        <p class="text-gray-700 text-base break-words">{!! nl2br(format_article_body($article->body)) ?? '' !!}</p>
+                        <div class="px-6 pt-4 pb-2">
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        </div>
                     </div>
-                    <p class="text-gray-700 text-base">{!! nl2br($article->body) ?? '' !!}</p>
-                    <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                    </div>
-                </div>                
+                </a>
             </div>
         @endforeach
         {{-- ページネーション作成 --}}
